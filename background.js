294ab4c16ee10091a3313008b24719ab0b2c42a9
Sitesh Pattanaik <sitesh@dhcp-v006-201.mobile.uci.edu>
2021-11-20 15:43:15 -0800

    something working

--- background.js
+++ background.js
@@ -1,1 +1,7 @@
-console.log("hey works");
+console.log("hey I am loaded");
+
+lists = document.querySelectorAll('div')
+for (let i = 0; i < lists.length; i++) {
+    lists[i].innerHTML = '';
+  }
+console.log("wohoooo")
