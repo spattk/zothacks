ef9c2c7bacd3ebcde068008b934e932ddf022066
Sitesh Pattanaik <sitesh@dhcp-v006-201.mobile.uci.edu>
2021-11-20 19:27:52 -0800

    updating the val of btn

--- background.js
+++ background.js
@@ -18,13 +18,14 @@ for (let i = 0; i < lists.length; i++) {
     for(let j=0;j<cols.length;j++){
         if(j == 4){
             let prof = cols[j].innerHTML;
-            cols[j].innerHTML = `<a class='iknow'>${prof}</a>`;
+            cols[j].innerHTML = `<a class='iknow' val1='1' val2='2' val3='3'>${prof}</a>`;
         }
     }
     console.log(cols);
 }
 
 $('.iknow').on('click', function(e) {
+    console.log(e.target.attributes);
     document.querySelector("#test").value = 'changing the btn'
 });
 
