3f7fd93a8ef06ad66c45227c17f09737221016dc
Sitesh Pattanaik <sitesh@dhcp-v006-201.mobile.uci.edu>
2021-11-20 17:19:29 -0800

    empty instructor

--- background.js
+++ background.js
@@ -1,7 +1,20 @@
 console.log("hey I am loaded");
 
-lists = document.querySelectorAll('div')
+// lists = document.querySelectorAll('div')
+// for (let i = 0; i < lists.length; i++) {
+//     lists[i].innerHTML = '';
+// }
+
+lists = document.querySelectorAll("tr[valign='top']");
+
 for (let i = 0; i < lists.length; i++) {
-    lists[i].innerHTML = '';
-  }
+    var cols = lists[i].childNodes;
+    for(let j=0;j<cols.length;j++){
+        if(j == 4){
+            cols[j].innerHTML = '';
+        }
+    }
+    console.log(cols);
+}
+
 console.log("wohoooo")
